a05a0b05850ff54ab9b12f466659eb4dea7e35ef
Asim J <asimjawahir123@gmail.com>
2022-01-30 09:27:08 +0300

    Included Probability in Lung Detection

--- Lungs_Prediction.py
+++ Lungs_Prediction.py
@@ -13,12 +13,13 @@ def predict_image(imagePath):
     img = np.expand_dims(img, axis=0)
 
     img = np.array(img) / 255.0
-
+    temp = (model.predict(img))
     results=(model.predict(img) > 0.5).astype("int32")
-    
+    # # pred_main = [0][0]
+    # # print(pred)
+    # print(temp[0][0])
     if results[0][0] == 0:
-        prediction = 'Positive For Covid-19'
+        prediction = 'Positive For Covid-19' + '\n' + 'Probability: ' + str("%.2f"%(100-round(temp[0][0]*100,2))) + '%'
     else:
-        prediction = 'Negative for Covid-19'
-    
+        prediction = 'Negative for Covid-19' + '\n' + 'Probability: ' + str("%.2f"%round(temp[0][0]*100,2)) + '%'
     return prediction
